12defc1f46733724ddd011b810cdd16dfab5580f
허승빈 <qmfhfl02@gmail.com>
2026-05-29 15:20:06 +0900

    fix

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ let API_KEY;
 function setup() {
   createCanvas(400, 400);
   rectMode(CENTER);
-  if(config && config.API_KEY){
+  if(config){
     API_KEY = config.API_KEY;
   } else {
     API_KEY = prompt("API 키를 입력해주세요.");
